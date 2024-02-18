91baa2f0659f5d30f1f0d88d9d3a0c495a140fda
Vincent Boon <github@vincentbean.com>
2024-02-18 21:22:24 +0100

    Setup core package

---
+++ packages/core/tests/TestCase.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Vigilant\Core\Tests;
+
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
+use Orchestra\Testbench\TestCase as BaseTestCase;
+use Vigilant\Core\ServiceProvider;
+
+abstract class TestCase extends BaseTestCase
+{
+    use LazilyRefreshDatabase;
+
+    protected function getPackageProviders($app): array
+    {
+        return [
+            ServiceProvider::class,
+        ];
+    }
+
+    protected function defineEnvironment($app): void
+    {
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+    }
+}
