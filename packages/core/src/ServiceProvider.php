b6a66b0c4ed80784fca52aacb6df74590290d77e
Vincent Boon <vincent@justbetter.nl>
2024-02-18 21:22:24 +0100

    Setup core package

---
+++ packages/core/src/ServiceProvider.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Vigilant\Core;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+    public function register(): void
+    {
+        $this
+            ->registerConfig()
+            ->registerActions();
+    }
+
+    protected function registerConfig(): static
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/core.php', 'core');
+
+        return $this;
+    }
+
+    protected function registerActions(): static
+    {
+
+        return $this;
+    }
+
+    public function boot(): void
+    {
+        $this
+            ->bootConfig()
+            ->bootMigrations()
+            ->bootCommands()
+            ->bootViews()
+            ->bootRoutes();
+    }
+
+    protected function bootConfig(): static
+    {
+        $this->publishes([
+            __DIR__.'/../config/core.php' => config_path('core.php'),
+        ], 'config');
+
+        return $this;
+    }
+
+    protected function bootMigrations(): static
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        return $this;
+    }
+
+    protected function bootCommands(): static
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+
+            ]);
+        }
+
+        return $this;
+    }
+
+    protected function bootViews(): static
+    {
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'core');
+
+        return $this;
+    }
+
+    protected function bootRoutes(): static
+    {
+        if (! $this->app->routesAreCached()) {
+            Route::middleware(['web', 'auth'])
+                ->group(fn () => $this->loadRoutesFrom(__DIR__.'/../routes/web.php'));
+        }
+
+        return $this;
+    }
+}
