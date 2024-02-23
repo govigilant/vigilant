5a7e1b3da7339f9a5eccc5d3c7fe72cf92a53269
Vincent Boon <vincent@justbetter.nl>
2024-02-23 07:52:01 +0100

    Aggregations

---
+++ packages/uptime/database/migrations/2024_02_22_073000_create_uptime_results_aggregates_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Vigilant\Uptime\Models\Monitor;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('uptime_results_aggregates', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Monitor::class)->index();
+
+            $table->unsignedFloat('total_time');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('uptime_results_aggregates');
+    }
+};
