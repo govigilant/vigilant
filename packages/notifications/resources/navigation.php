f9d478aa69c7dc55f1637b16a38d72b111ba6502
Vincent Boon <github@vincentbean.com>
2024-04-03 07:15:30 +0200

    Site table

--- packages/notifications/resources/navigation.php
+++ packages/notifications/resources/navigation.php
@@ -5,9 +5,9 @@ use Vigilant\Core\Facades\Navigation;
 Navigation::add(route('notifications'), 'Notifications')
     ->icon('tni-exclamation-circle-o')
     ->sort(400)
-    ->children(function() {
+    ->children(function(\Vigilant\Core\Navigation\Navigation $navigation): void {
 
-        Navigation::add(route('notifications.channels'), 'Notification Channels')
+        $navigation->add(route('notifications.channels'), 'Notification Channels')
             ->sort(401); // TODO: Fix child menu
 
     });
--- resources/navigation.php
+++ resources/navigation.php
@@ -4,13 +4,3 @@ use Vigilant\Core\Facades\Navigation;
 
 Navigation::add(route('dashboard'), 'Dashboard')
     ->icon('tni-area-chart-alt-o');
-
-Navigation::add(route('profile.show'), 'Profile')
-    ->icon('tni-user-o')
-    ->sort(20)
-    ->children(function (Vigilant\Core\Navigation\Navigation $nav) {
-
-        //$nav->add(route('profile.show'), 'test');
-        //$nav->add(route('profile.show'), 'test');
-
-    });
