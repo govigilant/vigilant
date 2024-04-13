c3f95f86b93711b9af31471e0c3f0b6ae01aec42
Vincent Boon <vincent@justbetter.nl>
2024-04-13 21:43:58 +0200

    Notification conditions

---
+++ packages/notifications/tests/Conditions/ConditionEngineTest.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace Vigilant\Notifications\Tests\Conditions;
+
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use Vigilant\Notifications\Conditions\ConditionEngine;
+use Vigilant\Notifications\Enums\Operator;
+use Vigilant\Notifications\Facades\NotificationRegistry;
+use Vigilant\Notifications\Tests\Fakes\Conditions\FalseCondition;
+use Vigilant\Notifications\Tests\Fakes\Conditions\TrueCondition;
+use Vigilant\Notifications\Tests\Fakes\FakeNotification;
+use Vigilant\Notifications\Tests\TestCase;
+
+class ConditionEngineTest extends TestCase
+{
+    #[Test]
+    #[DataProvider('conditions')]
+    public function it_checks_conditions(array $groups, string $operator, bool $expected): void
+    {
+        NotificationRegistry::registerCondition([
+            TrueCondition::class,
+            FalseCondition::class,
+        ]);
+
+        /** @var ConditionEngine $engine */
+        $engine = app(ConditionEngine::class);
+
+        $this->assertEquals($expected, $engine->checkGroup(
+            FakeNotification::make(1),
+            $groups,
+            $operator
+        ));
+    }
+
+    public static function conditions(): array
+    {
+        return [
+            'No conditions' => [
+                'groups' => [],
+                'operator' => 'all',
+                'excpected' => true,
+            ],
+
+            'True' => [
+                'groups' => [
+                    'type' => 'group',
+                    'children' => [
+                        [
+                            'type' => 'condition',
+                            'condition' => TrueCondition::class,
+                            'operator' => '=',
+
+                        ],
+                    ],
+                ],
+                'operator' => 'all',
+                'excpected' => true,
+            ],
+
+            'False' => [
+                'groups' => [
+                    'type' => 'group',
+                    'children' => [
+                        [
+                            'type' => 'condition',
+                            'condition' => FalseCondition::class,
+                            'operator' => '=',
+
+                        ],
+                    ],
+                ],
+                'operator' => 'all',
+                'excpected' => false,
+            ],
+
+            'Any' => [
+                'groups' => [
+                    'type' => 'group',
+                    'children' => [
+                        [
+                            'type' => 'condition',
+                            'condition' => FalseCondition::class,
+                            'operator' => '=',
+
+                        ],
+                        [
+                            'type' => 'condition',
+                            'condition' => TrueCondition::class,
+                            'operator' => '=',
+
+                        ],
+                    ],
+                ],
+                'operator' => 'any',
+                'excpected' => true,
+            ],
+
+            'All' => [
+                'groups' => [
+                    'type' => 'group',
+                    'children' => [
+                        [
+                            'type' => 'condition',
+                            'condition' => TrueCondition::class,
+                            'operator' => '=',
+
+                        ],
+                        [
+                            'type' => 'condition',
+                            'condition' => FalseCondition::class,
+                            'operator' => '=',
+
+                        ],
+                    ],
+                ],
+                'operator' => 'all',
+                'excpected' => false,
+            ],
+
+            'Children' => [
+                'groups' => [
+                    'type' => 'group',
+                    'children' => [
+                        [
+                            'type' => 'group',
+                            'operator' => 'all',
+                            'children' => [
+                                [
+                                    'type' => 'condition',
+                                    'condition' => TrueCondition::class,
+                                    'operator' => '=',
+                                ],
+                                [
+                                    'type' => 'condition',
+                                    'condition' => TrueCondition::class,
+                                    'operator' => '=',
+                                ],
+                            ],
+                        ],
+                        [
+                            'type' => 'group',
+                            'operator' => 'all',
+                            'children' => [
+                                [
+                                    'type' => 'condition',
+                                    'condition' => TrueCondition::class,
+                                    'operator' => '=',
+                                ],
+                                [
+                                    'type' => 'group',
+                                    'operator' => 'any',
+                                    'children' => [
+                                        [
+                                            'type' => 'condition',
+                                            'condition' => FalseCondition::class,
+                                            'operator' => '=',
+                                        ],
+                                        [
+                                            'type' => 'condition',
+                                            'condition' => TrueCondition::class,
+                                            'operator' => '=',
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                'operator' => 'all',
+                'excpected' => true,
+            ],
+        ];
+    }
+}
---
+++ packages/notifications/tests/Fakes/Conditions/TrueCondition.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Vigilant\Notifications\Tests\Fakes\Conditions;
+
+use Vigilant\Notifications\Conditions\Condition;
+use Vigilant\Notifications\Notifications\Notification;
+
+class TrueCondition extends Condition
+{
+    public function applies(Notification $notification, ?string $operand, string $operator, mixed $value, ?array $meta): bool
+    {
+        return true;
+    }
+}
