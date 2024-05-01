10b5b06fa831522f924851cffdb75d734cae72ef
Vincent Boon <vincent@justbetter.nl>
2024-05-01 21:17:40 +0200

    Set team for notification

--- packages/uptime/src/Listeners/DowntimeStartNotificationListener.php
+++ packages/uptime/src/Listeners/DowntimeStartNotificationListener.php
@@ -2,13 +2,20 @@
 
 namespace Vigilant\Uptime\Listeners;
 
+use Vigilant\Core\Services\TeamService;
 use Vigilant\Uptime\Events\DowntimeStartEvent;
 use Vigilant\Uptime\Notifications\DowntimeStartNotification;
 
 class DowntimeStartNotificationListener
 {
+    public function __construct(protected TeamService $teamService)
+    {
+    }
+
     public function handle(DowntimeStartEvent $event): void
     {
+        $this->teamService->setTeam($event->monitor->team);
+
         DowntimeStartNotification::notify($event->monitor);
     }
 }
