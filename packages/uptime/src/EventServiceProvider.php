7d6c7ad9127a079c6bc11fb1a00d75a986af0c75
Vincent Boon <github@vincentbean.com>
2024-05-01 21:03:23 +0200

    Event service provider

---
+++ packages/uptime/src/EventServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Vigilant\Uptime;
+
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+use Vigilant\Uptime\Events\DowntimeEndEvent;
+use Vigilant\Uptime\Events\DowntimeStartEvent;
+use Vigilant\Uptime\Listeners\DowntimeEndNotificationListener;
+use Vigilant\Uptime\Listeners\DowntimeStartNotificationListener;
+
+class EventServiceProvider extends ServiceProvider
+{
+    protected $listen = [
+        DowntimeStartEvent::class => [
+            DowntimeStartNotificationListener::class,
+        ],
+         DowntimeEndEvent::class => [
+            DowntimeEndNotificationListener::class,
+        ],
+    ];
+}
