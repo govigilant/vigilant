b26ff7639e10ad16b2c0bd88b9990d068f2927ec
Vincent Boon <github@vincentbean.com>
2024-05-16 21:25:07 +0200

    Lighthouse tests

---
+++ packages/lighthouse/tests/Actions/LighthouseTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Vigilant\Lighthouse\Tests\Actions;
+
+use Illuminate\Support\Facades\Process;
+use Mockery\MockInterface;
+use PHPUnit\Framework\Attributes\Test;
+use Vigilant\Lighthouse\Actions\CheckLighthouseResult;
+use Vigilant\Lighthouse\Actions\Lighthouse;
+use Vigilant\Lighthouse\Models\LighthouseResult;
+use Vigilant\Lighthouse\Models\LighthouseSite;
+use Vigilant\Lighthouse\Tests\TestCase;
+
+class LighthouseTest extends TestCase
+{
+    #[Test]
+    public function it_runs_lighthouse(): void
+    {
+        $this->mock(CheckLighthouseResult::class, function(MockInterface $mock): void {
+            $mock->shouldReceive('check')->once();
+        });
+
+        $output = [
+            'categories' => [
+                'performance' => [
+                    'score' => 1,
+                ],
+                'accessibility' => [
+                    'score' => 1,
+                ],
+                'best-practices' => [
+                    'score' => 1,
+                ],
+                'seo' => [
+                    'score' => 1,
+                ],
+            ],
+        ];
+
+        Process::fake([
+            '*' => Process::result(
+                output: json_encode($output),
+            )
+        ]);
+
+        /** @var Lighthouse $action */
+        $action = app(Lighthouse::class);
+
+        /** @var LighthouseSite $site */
+        $site = LighthouseSite::query()->create([
+            'team_id' => 1,
+            'url' => 'https://govigilant.io',
+            'settings' => [],
+            'interval' => '0 * * * *',
+        ]);
+
+        $action->run($site);
+
+        /** @var ?LighthouseResult $result */
+        $result = $site->lighthouseResults()->first();
+
+        $this->assertNotNull($result);
+        $this->assertEquals(1, $result->performance);
+        $this->assertEquals(1, $result->accessibility);
+        $this->assertEquals(1, $result->best_practices);
+        $this->assertEquals(1, $result->seo);
+    }
+}
---
+++ packages/lighthouse/tests/Data/CategoryResultDifferenceDataTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Vigilant\Lighthouse\Tests\Data;
+
+use PHPUnit\Framework\Attributes\Test;
+use Vigilant\Lighthouse\Data\CategoryResultDifferenceData;
+use Vigilant\Lighthouse\Tests\TestCase;
+
+class CategoryResultDifferenceDataTest extends TestCase
+{
+    #[Test]
+    public function test_calculations(): void
+    {
+        $data = CategoryResultDifferenceData::of([
+            'performance_old' => 0.5,
+            'performance_new' => 1,
+            'accessibility_old' => 1,
+            'accessibility_new' => 0.5,
+            'best_practices_old' => 1,
+            'best_practices_new' => 1,
+            'seo_old' => 0,
+            'seo_new' => 0,
+        ]);
+
+       $this->assertEquals(100, $data->performanceDifference());
+       $this->assertEquals(-50, $data->accessibilityDifference());
+       $this->assertEquals(0, $data->bestPracticesDifference());
+       $this->assertEquals(0, $data->seoDifference());
+    }
+}
