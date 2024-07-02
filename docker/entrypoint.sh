540c114445c555e4afc425fb11fec71fbc0baff7
Vincent Boon <vincent@justbetter.nl>
2024-07-02 19:13:29 +0200

    Adjust image workflow

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,4 +6,8 @@ php artisan notifications:rename-classes
 
 cp -f -r /tmp/public/* /app/public
 
+if ! grep -q "^APP_KEY=" ".env" || [ -z "$(grep "^APP_KEY=" ".env" | cut -d '=' -f2)" ]; then
+    php artisan key:generate
+fi
+
 /usr/bin/supervisord -c /app/docker/supervisor/supervisor.conf
