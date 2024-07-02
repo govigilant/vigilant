fe08feff143351b1458a2b75419bded6cbd75eb3
Vincent Boon <github@vincentbean.com>
2024-07-02 19:29:04 +0200

    Adjust entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
-php artisan migrate --force
-php artisan notifications:create
-php artisan notifications:rename-classes
-
 cp -f -r /tmp/public/* /app/public
+
+mkdir -p /app/storage/framework/cache
+mkdir -p /app/storage/framework/sessions
+mkdir -p /app/storage/framework/views
+
+if [ ! -f "/app/.env" ]; then
+    cp /app/.env.docker /app/.env
+fi
 
 if ! grep -q "^APP_KEY=" ".env" || [ -z "$(grep "^APP_KEY=" ".env" | cut -d '=' -f2)" ]; then
     php artisan key:generate
 fi
+
+php artisan migrate --force
+php artisan notifications:create
+php artisan notifications:rename-classes
 
 /usr/bin/supervisord -c /app/docker/supervisor/supervisor.conf
