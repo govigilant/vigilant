d25313a3b7d1456503f3404483a3ee0d94af8ac7
Vincent Boon <vincent@justbetter.nl>
2024-08-02 20:02:25 +0200

    Fix lighthouse result audit

--- packages/lighthouse/src/Actions/CheckLighthouseResultAudit.php
+++ packages/lighthouse/src/Actions/CheckLighthouseResultAudit.php
@@ -2,6 +2,7 @@
 
 namespace Vigilant\Lighthouse\Actions;
 
+use Illuminate\Database\Query\JoinClause;
 use Vigilant\Lighthouse\Models\LighthouseResultAudit;
 use Vigilant\Lighthouse\Notifications\NumericAuditChangedNotification;
 
@@ -12,15 +13,14 @@ class CheckLighthouseResultAudit
         if ($audit->scoreDisplayMode !== 'numeric') {
             return;
         }
+        $current = $this->averageNumericValue($audit, 10, 0);
 
-        $current = $this->averageNumericValue($audit, 4, 0);
-
-        $previous = $this->averageNumericValue($audit, 12, 4);
+        $previous = $this->averageNumericValue($audit, 50, 10);
 
         if ($previous == 0) {
             $percentDifference = ($current == 0) ? 0 : 100;
         } else {
-            $percentDifference = (($previous - $current) / $previous) * 100;
+            $percentDifference = (($current - $previous) / $previous) * 100;
         }
 
         if ($percentDifference > 0) {
@@ -31,9 +31,13 @@ class CheckLighthouseResultAudit
     protected function averageNumericValue(LighthouseResultAudit $audit, int $count = 3, int $skip = 0): float
     {
         return (float) LighthouseResultAudit::query()
-            ->where('lighthouse_result_id', '=', $audit->lighthouse_result_id)
+            ->join('lighthouse_results', function (JoinClause $join) use ($audit) {
+                $join->on('lighthouse_results.id', '=', 'lighthouse_result_audits.lighthouse_result_id')
+                    ->where('lighthouse_result.lighthouse_monitor_id', '=',
+                        $audit->lighthouseResult->lighthouse_monitor_id);
+            })
             ->where('audit', '=', $audit->audit)
-            ->orderByDesc('id')
+            ->orderByDesc('lighthouse_result_audits.id')
             ->skip($skip)
             ->take($count)
             ->get()
