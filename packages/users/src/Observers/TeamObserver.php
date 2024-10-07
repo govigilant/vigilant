1326900c2d86eade11e5e5e1a24f101d61bdb4d8
Vincent Boon <vincent@justbetter.nl>
2024-10-07 19:02:37 +0200

    SCA

--- packages/users/src/Observers/TeamObserver.php
+++ packages/users/src/Observers/TeamObserver.php
@@ -4,16 +4,15 @@ namespace Vigilant\Users\Observers;
 
 use Illuminate\Database\Eloquent\Model;
 use Vigilant\Core\Services\TeamService;
-use Vigilant\Users\Models\User;
 
 class TeamObserver
 {
-    /** @param User $model */
     public function creating(Model $model): void
     {
         /** @var TeamService $teamService */
         $teamService = app(TeamService::class);
 
+        // @phpstan-ignore-next-line
         $model->team_id = $teamService->team()?->id;
     }
 }
