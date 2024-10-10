08b8f36090e8c7fa29a54cc0ab2dad2c68b6878c
Vincent Boon <github@vincentbean.com>
2024-10-10 18:16:49 +0200

    Add policy to site model

--- resources/views/components/create-button.blade.php
+++ resources/views/components/create-button.blade.php
@@ -4,8 +4,12 @@
         {{ $slot }}
     </x-form.button>
 @else
-    <x-form.button {{ $attributes->merge(['class' => 'bg-blue/70 hover:bg-blue/50 cursor-not-allowed'])->except(['href']) }} disabled>
-        {{ $slot }}
+    <x-form.button {{ $attributes->merge(['class' => 'bg-blue/70 hover:bg-blue/50 cursor-not-allowed has-tooltip'])->except(['href']) }} disabled>
+        <span class="tooltip rounded shadow-lg bg-base-950 text-base-100 mt-8 prose px-2 py-1 text-xs leading-2">
+           @lang('Your current plan does not allow to create this resource')
+        </span>
+        <span>
+            {{ $slot }}
+        </span>
     </x-form.button>
 @endcan
-
