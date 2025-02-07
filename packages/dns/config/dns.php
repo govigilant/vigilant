905241d51f9abe3aa5574713cca6b7c7e5d0bea0
Vincent Boon <github@vincentbean.com>
2025-02-07 21:23:42 +0100

    Support multiple DNS servers (#19)

--- packages/dns/config/dns.php
+++ packages/dns/config/dns.php
@@ -3,5 +3,7 @@
 return [
     'queue' => 'dns',
 
-    'nameserver' => '1.1.1.1',
+    'nameservers' => env('DNS_NAMESERVERS', '1.1.1.1,1.0.0.1,9.9.9.9,8.8.8.8'),
+
+    'max_attempts' => env('DNS_MAX_ATTEMPTS', 3),
 ];
--- packages/dns/src/Client/DnsClient.php
+++ packages/dns/src/Client/DnsClient.php
@@ -8,17 +8,35 @@ use BlueLibraries\Dns\Handlers\Types\TCP;
 
 class DnsClient
 {
-    public function get(string $record, int|array $type): array
+    public function get(string $record, int|array $type, int $attempt = 0): array
     {
+        /** @var int $maxAttempts */
+        $maxAttempts = config('dns.max_attempts', 3);
+
+        if ($attempt > $maxAttempts) {
+            return [];
+        }
+
         $dnsHandler = (new TCP)
-            ->setNameserver(config('dns.nameserver'));
+            ->setNameserver($this->getNameserver())
+            ->setTimeout(3);
 
         $dnsRecordsService = new DnsRecords($dnsHandler);
 
         try {
             return $dnsRecordsService->get($record, $type);
         } catch (DnsHandlerException $e) {
-            return [];
+            logger()->error("Failed to retrieve DNS record $record on attempt $attempt: ".$e->getMessage());
+
+            return $this->get($record, $type, $attempt + 1);
         }
+    }
+
+    protected function getNameserver(): string
+    {
+        /** @var ?string $nameservers */
+        $nameservers = config('dns.nameservers');
+
+        return str($nameservers)->explode(',')->random();
     }
 }
