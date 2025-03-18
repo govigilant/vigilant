b516043dc72cf3f7e4840f77c207d749055c8582
Vincent Bean <github@vincentbean.com>
2025-03-18 20:27:08 +0100

    Styling

--- resources/views/components/form/submit-button.blade.php
+++ resources/views/components/form/submit-button.blade.php
@@ -1,7 +1,7 @@
 <div class="flex justify-end gap-2">
     {{ $slot }}
     <button type="submit"
-        {{ $attributes->merge(['class' => 'rounded-full bg-red hover:bg-red-light px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-light disabled:opacity-50']) }}>
+        {{ $attributes->merge(['class' => 'rounded-md bg-red hover:bg-red-light px-4 py-2 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-light disabled:opacity-50']) }}>
         {{ __($submitText ?? 'Create') }}
     </button>
 </div>
--- resources/views/components/page-header.blade.php
+++ resources/views/components/page-header.blade.php
@@ -1,15 +1,15 @@
 @props(['title', 'back'])
 <div class="md:flex md:items-center md:justify-between">
     <div class="min-w-0 flex-1 flex items-center gap-4">
-        @if(isset($back))
+        @if (isset($back))
             <a href="{{ $back }}" wire:navigate.hover>
                 @svg('tni-arrow-left-circle-o', 'w-6 h-6')
             </a>
         @endif
-        <h2 class="text-xl font-bold leading-7 sm:truncate sm:text-2xl sm:tracking-tight text-neutral-100">{{ __($title) }}</h2>
+        <h2 class="text-xl font-bold leading-7 sm:truncate sm:text-2xl sm:tracking-tight text-neutral-100">
+            {{ __($title) }}</h2>
     </div>
-    <div class="mt-4 flex md:ml-4 md:mt-0">
+    <div class="mt-4 flex md:ml-4 md:mt-0 gap-4">
         {{ $slot }}
     </div>
 </div>
-
