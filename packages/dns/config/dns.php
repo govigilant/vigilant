4742f251e5647adb50f9b87c22faa9d3aae4ec56
Vincent Bean <github@vincentbean.com>
2025-04-01 21:35:54 +0200

    Cast

--- packages/dns/config/dns.php
+++ packages/dns/config/dns.php
@@ -5,5 +5,5 @@ return [
 
     'nameservers' => env('DNS_NAMESERVERS', '1.1.1.1,1.0.0.1,9.9.9.9,8.8.8.8'),
 
-    'max_attempts' => env('DNS_MAX_ATTEMPTS', 3),
+    'max_attempts' => (int) env('DNS_MAX_ATTEMPTS', 3),
 ];
