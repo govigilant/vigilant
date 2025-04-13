23ef74fc76b3837bf23fcd7b32f0852f157f17b3
Vincent Bean <github@vincentbean.com>
2025-04-13 14:34:19 +0200

    Adjust site tabs

--- packages/sites/resources/views/livewire/form.blade.php
+++ packages/sites/resources/views/livewire/form.blade.php
@@ -19,9 +19,7 @@
                             <select name="tabs" x-model="selectedTab"
                                 class="block w-full rounded-md border-none bg-white/5 py-2 pl-3 pr-10 text-base text-white shadow-xs ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm">
                                 @foreach ($tabs as $key => $data)
-                                    @can('create', $data['model'])
-                                        <option value="{{ $key }}">{{ $data['title'] }}</option>
-                                    @endcan
+                                    <option value="{{ $key }}">{{ $data['title'] }}</option>
                                 @endforeach
                             </select>
                         </div>
@@ -29,14 +27,14 @@
                             <nav class="flex border-b border-base-700">
                                 <ul class="flex min-w-full gap-x-2 text-sm font-semibold text-gray-400">
                                     @foreach ($tabs as $key => $data)
-                                        @can('create', $data['model'])
-                                            <li x-on:click="selectedTab = '{{ $key }}'"
-                                                :class="{ 'text-red bg-base-800 rounded-t-lg': selectedTab ==
-                                                        '{{ $key }}' }"
-                                                class="cursor-pointer select-none px-2.5 py-1.5 hover:bg-base-800 hover:rounded-t-lg">
-                                                {{ $data['title'] }}
-                                            </li>
-                                        @endcan
+                                        <li x-on:click="selectedTab = '{{ $key }}'"
+                                            :class="{
+                                                'text-red bg-base-800 rounded-t-lg': selectedTab ==
+                                                    '{{ $key }}'
+                                            }"
+                                            class="cursor-pointer select-none px-2.5 py-1.5 hover:bg-base-800 hover:rounded-t-lg">
+                                            {{ $data['title'] }}
+                                        </li>
                                     @endforeach
                                 </ul>
                             </nav>
