471aeaa1a5dbfed75bc128e0eb38759ea87d5769
Vincent Bean <github@vincentbean.com>
2025-06-06 21:15:11 +0200

    SCA

--- packages/uptime/src/Http/Livewire/UptimeMonitorForm.php
+++ packages/uptime/src/Http/Livewire/UptimeMonitorForm.php
@@ -34,6 +34,7 @@ class UptimeMonitorForm extends Component
             $this->monitor = $monitor;
         }
 
+        /** @var array<int, int> $intervals */
         $availableIntervals = array_keys(config('uptime.intervals', []));
 
         if (! in_array($this->form->interval, $availableIntervals) && count($availableIntervals) > 0) {
