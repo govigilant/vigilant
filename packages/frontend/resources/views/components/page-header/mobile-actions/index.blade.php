c468620350da7cd77f739e8593a8c8212c949830
Vincent Bean <github@vincentbean.com>
2025-11-01 18:54:36 +0100

    Z index

--- packages/frontend/resources/views/components/page-header/mobile-actions/index.blade.php
+++ packages/frontend/resources/views/components/page-header/mobile-actions/index.blade.php
@@ -1,14 +1,16 @@
 <div class="lg:hidden" x-data="{ open: false }">
     <button
+        x-ref="trigger"
         class="group relative flex items-center justify-center rounded-lg px-4 py-2.5 bg-base-900/50 border border-base-800/50 text-base-300 transition-all duration-200 hover:bg-base-800/50 hover:border-base-700 hover:text-base-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red"
         type="button" x-on:click="open = !open">
         @svg('tni-menu-o', 'size-6 transition-colors duration-200')
     </button>
     <div
-        class="absolute z-50 right-4 mt-2 w-56 overflow-hidden rounded-lg bg-base-950 border border-base-800/50 shadow-xl"
+        class="fixed z-[100] right-4 mt-2 w-56 overflow-hidden rounded-lg bg-base-950 border border-base-800/50 shadow-xl"
         x-show="open" 
         @click.away="open = false" 
         x-cloak
+        x-anchor.bottom-end="$refs.trigger"
         x-transition:enter="transition ease-out duration-200"
         x-transition:enter-start="opacity-0 scale-95"
         x-transition:enter-end="opacity-100 scale-100"
