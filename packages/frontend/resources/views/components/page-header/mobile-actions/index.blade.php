83a5c92cd0b100dfc1accd099299faeb700e7103
Vincent Bean <github@vincentbean.com>
2025-11-01 20:36:36 +0100

    Z index 2

--- packages/frontend/resources/views/components/page-header/mobile-actions/index.blade.php
+++ packages/frontend/resources/views/components/page-header/mobile-actions/index.blade.php
@@ -5,20 +5,23 @@
         type="button" x-on:click="open = !open">
         @svg('tni-menu-o', 'size-6 transition-colors duration-200')
     </button>
-    <div
-        class="fixed z-[100] right-4 mt-2 w-56 overflow-hidden rounded-lg bg-base-950 border border-base-800/50 shadow-xl"
-        x-show="open" 
-        @click.away="open = false" 
-        x-cloak
-        x-anchor.bottom-end="$refs.trigger"
-        x-transition:enter="transition ease-out duration-200"
-        x-transition:enter-start="opacity-0 scale-95"
-        x-transition:enter-end="opacity-100 scale-100"
-        x-transition:leave="transition ease-in duration-150"
-        x-transition:leave-start="opacity-100 scale-100"
-        x-transition:leave-end="opacity-0 scale-95">
-        <div class="py-1" role="menu">
-            {{ $slot }}
+    
+    <template x-teleport="body">
+        <div
+            x-show="open" 
+            @click.away="open = false" 
+            x-cloak
+            class="fixed z-[9999] w-56 overflow-hidden rounded-lg bg-base-950 border border-base-800/50 shadow-xl"
+            x-transition:enter="transition ease-out duration-200"
+            x-transition:enter-start="opacity-0 scale-95"
+            x-transition:enter-end="opacity-100 scale-100"
+            x-transition:leave="transition ease-in duration-150"
+            x-transition:leave-start="opacity-100 scale-100"
+            x-transition:leave-end="opacity-0 scale-95"
+            :style="`top: ${$refs.trigger?.getBoundingClientRect().bottom + 8}px; right: ${window.innerWidth - $refs.trigger?.getBoundingClientRect().right}px;`">
+            <div class="py-1" role="menu">
+                {{ $slot }}
+            </div>
         </div>
-    </div>
+    </template>
 </div>
