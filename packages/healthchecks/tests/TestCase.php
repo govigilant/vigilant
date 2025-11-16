f365b621426df6385927fd958d6eebe7f9febd5e
Vincent Boon <github@vincentbean.com>
2025-11-16 15:52:51 +0100

    Remove incomplete tests

--- packages/healthchecks/tests/TestCase.php
+++ packages/healthchecks/tests/TestCase.php
@@ -2,27 +2,40 @@
 
 namespace Vigilant\Healthchecks\Tests;
 
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
+use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as BaseTestCase;
+use Vigilant\Core\Services\TeamService;
 use Vigilant\Healthchecks\ServiceProvider;
 
 abstract class TestCase extends BaseTestCase
 {
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        // Additional setup
-    }
+    use LazilyRefreshDatabase;
 
     protected function getPackageProviders($app): array
     {
         return [
             ServiceProvider::class,
+            \Vigilant\Core\ServiceProvider::class,
+            \Vigilant\Users\ServiceProvider::class,
+            LivewireServiceProvider::class,
         ];
     }
 
-    protected function getEnvironmentSetUp($app): void
+    protected function defineEnvironment($app): void
     {
-        // Configure environment
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        TeamService::fake();
     }
 }
