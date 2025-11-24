46e913b6dc92126f91082274869708599b68213d
Vincent Boon <github@vincentbean.com>
2025-11-24 20:56:37 +0100

    Fix bursting

--- packages/notifications/src/Actions/CheckBurst.php
+++ packages/notifications/src/Actions/CheckBurst.php
@@ -2,21 +2,23 @@
 
 namespace Vigilant\Notifications\Actions;
 
+use Vigilant\Notifications\Models\Channel;
+use Vigilant\Notifications\Models\Trigger;
 use Vigilant\Notifications\Notifications\Notification;
 
 class CheckBurst
 {
     protected const int BURST_SECONDS = 30;
 
-    public function isBursting(Notification $notification): bool
+    public function isBursting(Notification $notification, Trigger $trigger, Channel $channel): bool
     {
-        $key = $this->cacheKey($notification);
+        $key = $this->cacheKey($notification, $trigger, $channel);
 
         return ! cache()->add($key, 1, self::BURST_SECONDS);
     }
 
-    protected function cacheKey(Notification $notification): string
+    protected function cacheKey(Notification $notification, Trigger $trigger, Channel $channel): string
     {
-        return sprintf('notifications:burst:%s:%s', $notification::class, $notification->uniqueId());
+        return sprintf('notifications:burst:%s:%s:%s:%s', $notification::class, $notification->uniqueId(), $trigger->id, $channel->id);
     }
 }
