70e48dbee831850030f1062d8afa5f0cd3955400
Vincent Boon <github@vincentbean.com>
2026-02-19 20:25:45 +0100

    Adjust uptime percentage, format latency

--- packages/uptime/src/Http/Livewire/Charts/ColumnLatencyChart.php
+++ packages/uptime/src/Http/Livewire/Charts/ColumnLatencyChart.php
@@ -27,7 +27,7 @@ class ColumnLatencyChart extends LatencyChart
 
     public function data(): array
     {
-        $points = $this->points()->pluck('total_time');
+        $points = $this->points()->pluck('total_time')->map(fn (float $time): int => round($time));
 
         return [
             'type' => 'line',
